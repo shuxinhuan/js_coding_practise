dc0fa6481e457c202e9bfefe713ce0df4048f998
Xinhuan Shu <vlanxin@hotmail.com>
2017-08-25 23:03:05 +0800

    backup

--- force_directed/force.js
+++ force_directed/force.js
@@ -1,20 +1,200 @@
-var WIDTH = 1000;
-var HEIGHT = 500;
+var WIDTH = 800;
+var HEIGHT = 800;
+var SIZE = 7;
 var colors = ["#FF7F0E", "#1F77B4", "#AEC7E8", "#98DF8A", "#2CA02C",
              "#9467BD", "#FFBB78", "#8C564B", "#FF9896", "#C5B0D5"];
+//used for the calculation of force
+var REPULSION_CONSTANT = 15000;
+var ATTRACTION_CONSTANT = 0.1;
+var SPRING_LENGTH = 50;
 
-function generateGraph(nodes, links, svgID){
-  //1. initially arrange nodes randomly
+var MAX_ITERATION = 150;
+
+
+var pos = [];
+var findByID = new Array();
+var edges = [];
+  //============1. initially arrange nodes randomly
+function initialGraph(nodes, links, svgID){
   for (var i in nodes){
-    var circle = Circle.createNew(Math.random()*WIDTH, Math.random()*HEIGHT, nodes[i].id, nodes[i].group);
-    var circleSVG = document.createElementNS("http://www.w3.org/2000/svg","circle");
-            circleSVG.setAttribute('cx', circle.x);
-            circleSVG.setAttribute('cy', circle.y);
-            circleSVG.setAttribute('r', 4);
-            circleSVG.setAttribute('style', "fill: " + colors[circle.group]);
-            $('#' + svgID).append(circleSVG);
+    pos[i] = Position.createNew(2*WIDTH/5 + Math.floor(Math.random() * (WIDTH/5)),
+                                     2*HEIGHT/5 + Math.floor(Math.random() * (HEIGHT/5)), nodes[i].id, nodes[i].group);
+    findByID.push(nodes[i].id);
+  }
+  for (var i in links){
+    edges[i] = Edge.createNew(findByID.indexOf(links[i].source), findByID.indexOf(links[i].target), links[i].value);
 
   }
+  draw(pos, svgID);
+}
+
+
+function reArrange(stopCount, timeInterval, links, svgID){
+  // var findByID = new Array();
+
+
+  // var pos = [];
+  //============1. initially arrange nodes randomly
+  // for (var i in nodes){
+  //   pos[i] = Position.createNew(2*WIDTH/5 + Math.floor(Math.random() * (WIDTH/5)),
+  //                                    2*HEIGHT/5 + Math.floor(Math.random() * (HEIGHT/5)), nodes[i].id, nodes[i].group);
+  // }
+  // DrawCircle(pos, svgID);
+
+  // var prevTime = +new Date();
+  // for ( var cnt = 0; cnt < MAX_ITERATION; cnt++){
+    // var thisTime = +new Date();
+    // var timeInterval = (thisTime - prevTime)/1000;
+    // prevTime = thisTime;
+    // console.log(timeInterval);
+
+    var totalDisplacement = 0;
+    // var flag = false;
+    for (var i in pos){
+        //=============2. calculate repulsion force for every other node
+        var netForce = Force.createNew(0, 0);
+        // console.log(netForce);
+        for (var j in pos){
+          if ( i == j)
+            continue;
+          var repulsionForce = CalcRepulsion(pos[i], pos[j]);
+          // console.log(repulsionForce);
+          netForce.plusForce(repulsionForce);
+          // console.log(netForce);
+        }
+        // console.log(netForce);
+        // =============3. calculate attraction force for links
+        for (var j in links){
+          var source = -1;
+          var target = -1;
+          if(pos[i].id == links[j].source){
+            source = i;
+            target = findByID.indexOf(links[j].target);
+          }
+          if(pos[i].id == links[j].target){
+            source = findByID.indexOf(links[j].source);
+            target = i;
+          }
+          if(source != -1 && target != -1){
+            var attractionForce = CalcAttraction(pos[source], pos[target]);
+            // console.log(attractionForce);
+            netForce.plusForce(attractionForce);
+            // console.log(netForce);
+          }
+        } // end for links
+        // console.log(netForce);
+
+        // ===========4. apply force to velocity and location
+        pos[i].changeVelocity(netForce, timeInterval);
+    }//end for each node
+
+    //move to new location
+    for (var i in pos){
+      pos[i].nextLocation(timeInterval).adjust(SIZE, WIDTH, HEIGHT);
+      // console.log(pos[i]);
+      totalDisplacement += CalcDistance(pos[i].nextX, pos[i].nextY, pos[i].x, pos[i].y);
+      pos[i].changeLocation();
+    }
+
+    if(totalDisplacement < 10) stopCount++;
+    console.log(totalDisplacement);
+  // }// end interation
+
+
+  // ================  5. draw
+  reDraw(pos, svgID);
+  // console.log(stopCount);
+  return stopCount;
+} // end function reArrange
+
+
+
+
+
+function CalcDistance(x1, y1, x2, y2){
+  return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+}
+
+function CalcRepulsion(p1, p2){
+  var proximity = Math.max(CalcDistance(p1.x, p1.y, p2.x, p2.y), 1);
+
+  var totalForce = REPULSION_CONSTANT / Math.pow(proximity, 2);
+  var diffX = p2.x - p1.x;
+  var diffY = p2.y - p1.y;
+  var xForce = -totalForce * diffX / proximity;
+  var yForce = -totalForce * diffY / proximity;
+  // console.log(xForce);
+  // console.log(yForce);
+  return Force.createNew(xForce, yForce);
+}
+
+
+function CalcAttraction(p1, p2){
+  var proximity = Math.max(CalcDistance(p1.x, p1.y, p2.x, p2.y), 1);
+
+  var totalForce = ATTRACTION_CONSTANT * Math.max(proximity - SPRING_LENGTH, 0);
+  var diffX = p2.x - p1.x;
+  var diffY = p2.y - p1.y;
+  var xForce = totalForce * diffX / proximity;
+  var yForce = totalForce * diffY / proximity;
+  return Force.createNew(xForce, yForce);
+}
+
+
+
+function draw(p, svgID){
+  for (var i in p){
+    var circle = Circle.createNew(p[i].x, p[i].y, p[i].id, p[i].group);
+    var circleSVG = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    circleSVG.setAttribute('id', 'node' + i);
+    circleSVG.setAttribute('cx', circle.x);
+    circleSVG.setAttribute('cy', circle.y);
+    circleSVG.setAttribute('r', SIZE);
+    circleSVG.setAttribute('style', "fill: " + colors[circle.group % 10]);
+    $('#' + svgID).append(circleSVG);
+  }
+
+  for (var i in edges){
+    var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute('id', 'line'+i);
+    $('#' + svgID).append(line);
+    $('#line' + i).attr({
+      'x1': p[edges[i].source].x,
+      'y1': p[edges[i].source].y,
+      'x2': p[edges[i].target].x,
+      'y2': p[edges[i].target].y,
+      'stroke-width': edges[i].value / 3,
+    });
+  }
+
+  $("circle").attr({
+    "stroke" : "#fff",
+    "stroke-width" : "1.5px"
+  });
+
+  $("line").attr({
+    'stroke': '#999',
+    'stroke-opacity': '0.6',
+  });
+}
+
+function reDraw(p, svgID){
+  for (var i in p){
+    $('#node' + i).attr({
+      'cx': p[i].x,
+      'cy': p[i].y,
+    });
+  } //end for
+
+  for (var i in edges){
+    $('#line' + i).attr({
+      'x1': p[edges[i].source].x,
+      'y1': p[edges[i].source].y,
+      'x2': p[edges[i].target].x,
+      'y2': p[edges[i].target].y,
+    });
+  }
+
 }
 
 function initialSVG(parentId, svgID){
--- force_directed/include.js
+++ force_directed/include.js
@@ -2,10 +2,99 @@
 var Circle = {
   createNew: function(x, y, id, group){
     var circle = {};
-    circle.x = x;
-    circle.y = y;
+    if( x < 0)
+      circle.x = 0;
+    else if ( x > WIDTH)
+      circle.x = WIDTH;
+    else {
+      circle.x = x;
+    }
+    if( y < 0)
+      circle.y = 0;
+    else if( y > HEIGHT)
+      circle.y = HEIGHT;
+    else {
+      circle.y = y;
+    }
     circle.id = id;
     circle.group = group;
     return circle;
+  }
+}
+
+var Position = {
+  createNew: function(x, y, id, group){
+    var position = {};
+    position.x = x;
+    position.y = y;
+    position.id = id;
+    position.group = group;
+    position.velocity = Velocity.createNew(0, 0);
+    position.nextX = position.x;
+    position.nextY = position.y;
+
+    position.changeVelocity = function(force, time){
+      position.velocity.xVelocity += force.xForce * time; // v = v0 + at
+      position.velocity.yVelocity += force.yForce * time; // v = v0 + at
+      return position;
+    }
+
+    position.nextLocation = function(time){
+      position.nextX = position.x + position.velocity.xVelocity * time; // x = x0 + vt
+      position.nextY = position.y + position.velocity.yVelocity * time; // x = x0 + vt
+      return position;
+    }
+
+    position.changeLocation = function(){
+      position.x = position.nextX;
+      position.y = position.nextY;
+      return position;
+    }
+
+    position.adjust = function(size, width, height){
+      var aX = position.nextX;
+      var aY = position.nextY;
+      if(aX > width - size) aX = width - size;
+      if(aX < size) aX = size;
+      if(aY > width - size) aY = height - size;
+      if(aY < size) aY = size;
+      position.nextX = parseInt(aX);
+      position.nextY = parseInt(aY);
+      return position;
+    }
+    return position;
+  }
+}
+
+var Velocity = {
+  createNew: function(xVelocity, yVelocity){
+    var velocity = {};
+    velocity.xVelocity = xVelocity;
+    velocity.yVelocity = yVelocity;
+    return velocity;
+  }
+}
+
+var Force = {
+  createNew: function(xForce, yForce){
+    var force = {};
+    force.xForce = xForce;
+    force.yForce = yForce;
+    force.plusForce = function(another){
+      force.xForce += another.xForce;
+      force.yForce += another.yForce;
+      return force;
+    }
+    return force;
+  }
+}
+
+var Edge = {
+  createNew: function(source, target, value){
+    var edge = {};
+    edge.source = source;
+    edge.target = target;
+    edge.value = value;
+    return edge;
   }
 }
